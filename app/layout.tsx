87ae6d9e5142308b467fe5f39a3d64b0a8dc3354
Wacewatch <wavewatchcontact@gmail.com>
2025-12-10 17:36:05 +0100

    Integrate Histats tracking in layout
    Added Histats tracking script to layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script"
 import "./globals.css"
 
 const _geist = Geist({ subsets: ["latin"] })
@@ -41,7 +42,35 @@ export default function RootLayout({
       <body className={`font-sans antialiased`}>
         {children}
         <Analytics />
+
+        {/* === HISTATS === */}
+        <Script id="histats-init" strategy="afterInteractive">
+          {`
+            var _Hasync = _Hasync || [];
+            _Hasync.push(['Histats.start', '1,4996171,4,0,0,0,00010000']);
+            _Hasync.push(['Histats.fasi', '1']);
+            _Hasync.push(['Histats.track_hits', '']);
+            (function() {
+              var hs = document.createElement('script');
+              hs.type = 'text/javascript';
+              hs.async = true;
+              hs.src = ('//s10.histats.com/js15_as.js');
+              (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+            })();
+          `}
+        </Script>
+
+        <noscript>
+          <a href="/" target="_blank">
+            <img
+              src="//sstatic1.histats.com/0.gif?4996171&101"
+              alt="free hit counter javascript"
+            />
+          </a>
+        </noscript>
+        {/* === END HISTATS === */}
       </body>
     </html>
   )
 }
+
